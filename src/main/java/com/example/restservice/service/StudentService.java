1c2d0ab86ada0aad04c2997150b7f9eb0d5ae265
mena <tsikiniainatsilavina@gmail.com>
2026-03-25 16:24:26 +0300

    ajout des methode dans StudentService

--- src/main/java/com/example/restservice/service/StudentService.java
+++ src/main/java/com/example/restservice/service/StudentService.java
@@ -1,4 +1,20 @@
 package com.example.restservice.service;
 
+import com.example.restservice.Controller.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentService {
+
+    private final List<Student> students = new ArrayList<>();
+
+    public List<Student> addStudents(List<Student> newStudents) {
+        students.addAll(newStudents);
+        return students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
 }
