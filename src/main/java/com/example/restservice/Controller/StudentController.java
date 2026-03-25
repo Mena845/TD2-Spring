dff47ed55285e75589820e5163a9a8db9ff3cf9b
mena <tsikiniainatsilavina@gmail.com>
2026-03-25 16:22:31 +0300

    midification de StudentController

--- src/main/java/com/example/restservice/Controller/StudentController.java
+++ src/main/java/com/example/restservice/Controller/StudentController.java
@@ -1,48 +1,43 @@
 package com.example.restservice.Controller;
 
+import com.example.restservice.BadRequestException;
+import com.example.restservice.StudentValidator;
+import com.example.restservice.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
-    private static List<Student> students = new ArrayList<>();
 
-    // a) GET /welcome?name
-    @GetMapping("/welcome")
-    public ResponseEntity<String> welcome(@RequestParam(required = false) String name) {
+    private final StudentService service = new StudentService();
+    private final StudentValidator validator = new StudentValidator();
 
-        if (name == null || name.isEmpty()) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST) // 400
-                    .body("Paramètre 'name' manquant");
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.OK) // 200
-                .body("Welcome " + name);
-    }
-    // B) POST /students
     @PostMapping("/students")
-    public List<Student> createStudents(@RequestBody List<Student> newStudents) {
-        students.addAll(newStudents);
+    public ResponseEntity<?> createStudents(@RequestBody List<Student> newStudents) {
 
-       return students;
-    }
+        try {
+            // Validation
+            validator.validate(newStudents);
 
-    // C) GET /students avec header Accept
-    @GetMapping("/students")
-    public String getStudents(@RequestHeader(name = "Accept") String header) {
+            // Service
+            List<Student> result = service.addStudents(newStudents);
 
-        if (header != null && !header.equals("text/plain") && !header.equals("application/json")) {
-            return students.stream()
-                    .map(s -> s.getFirstName() + " " + s.getLastName())
-                    .collect(Collectors.joining(", "));
-        } else {
-            return "Format non supporté";
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(result);
+
+        } catch (BadRequestException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erreur serveur");
         }
     }
 }
